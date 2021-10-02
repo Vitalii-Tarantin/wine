518359e6f66074ffb1e9042da5162c783f26d649
AlinaVinogradova <alina.vinogradova.2710@gmail.com>
2021-10-02 11:34:23 -0700

    header promo

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,26 @@
-// $('.mainMenu').each(function(){
-//   var actItem = $(this).find('.current-menu-item');
-//   if(!actItem){
-//     return;
-//   }
-//
-//   var line = $('<div class="actLine"></div>');
-//   $(this).append(line);
-//
-//   var setLineCss = function(node){
-//     line.css({
-//       width: node.css('width'),
-//       left: node[0].offsetLeft
-//     });
-//   };
-//
-//   //setLineCss(actItem);
-//
-//   $(this).on('mouseenter', 'li:not(.pll-parent-menu-item, .lang-item)', function(){
-//     setLineCss($(this));
-//   });
-//
-//   $(this).on('mouseleave', function(){
-//     setLineCss(actItem);
-//   });
-// });
+$('.wine__nav__list').each(function(){
+  var actItem = $(this).find('.act');
+  if(!actItem){
+    return;
+  }
+
+  var line = $('<div class="actLine"></div>');
+  $(this).append(line);
+
+  var setLineCss = function(node){
+    line.css({
+      width: node.css('width'),
+      left: node[0].offsetLeft
+    });
+  };
+
+  setLineCss(actItem);
+
+  $(this).on('mouseenter', 'li', function(){
+    setLineCss($(this));
+  });
+
+  $(this).on('mouseleave', function(){
+    setLineCss(actItem);
+  });
+});
